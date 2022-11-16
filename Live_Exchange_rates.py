3701af19a18968e7c85aba701cc89ef596051aef
Ujjwal Kumar <ujjwalkumar949494@gmail.com>
2022-11-17 00:13:41 +0530

    Add files via upload

---
+++ Live_Exchange_rates.py
@@ -0,0 +1,12 @@
+import urllib3
+import json
+url = "https://api.exchangerate.host/latest/"
+http = urllib3.PoolManager()
+response = http.request("GET",url)
+data = response.data
+currency = json.loads(data)
+currency_data = currency['rates']
+user = input("Enter a currency Code :").upper()
+for i,j in currency_data.items ():
+    if i == user:
+        print("Current Value = ",j)
